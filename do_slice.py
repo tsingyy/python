2a431e4d56d59c0ba859ae02bf4250c5018b319c
tsingyy <tsingyqx@163.com>
2016-12-16 14:04:52 +0800

    add slice

---
+++ do_slice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+L=['Ben','Herry','Jimmy','Lucy','Zoe']
+print('L=',L)
+print('L[0:2]=',L[0:2])
+print('L[:3]=',L[:3])
+print('L[1:4]=',L[1:4])
+print('L[-2:]=',L[-2:])
+
+R=list(range(40))
+print('R=',R)
+print('R[:10]=',R[:10])
+print('R[-10:]=',R[-10:])
+print('R(10:20)=',R[10:20])
+print('R[:10:2]=',R[:10:2])
+print('R[::5]=',R[::5])
